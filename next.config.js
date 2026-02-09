742f6f4e125425063229b4550112b623eb6a6ab7
vishstudio <vishstudio.ltd@gmail.com>
2026-02-10 03:15:17 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
-const repoName = 'nxt-moris-metrics-web';
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: '', 
+  assetPrefix: '', 
   images: {
     unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
-
